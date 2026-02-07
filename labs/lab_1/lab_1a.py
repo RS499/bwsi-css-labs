ca9cd4ff9815c5d3bc38dee71c504ec8ea8c3d10
RS499 <rajan.saha499@gmail.com>
2026-02-07 18:10:42 -0500

    Added personal introduction to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Rajan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("My name is Rajan and I'm a junior at Pennsylvania. I enjoy game dev, creating apps, playing soccer, running track, and playing the guitar in my free time. My favorite ice cream flavor is cookies and cream. I'm looking forward to getting know you guys and doing this course!");
 if __name__ == "__main__":
     main()
