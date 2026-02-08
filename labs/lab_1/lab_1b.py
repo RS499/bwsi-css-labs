41f15bb879a60d88bc27f4f97f8b9ba6ef5a9542
RS499 <rajan.saha499@gmail.com>
2026-02-08 13:07:36 -0500

    add user input sanitization

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,6 +8,21 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
+from prompt_toolkit import prompt
+
+
+def request_santized_number(prompt: str) -> float:
+    """H
+    Function to request and sanitize user input for the operation.
+    Returns:
+    float: The sanitized numeric input by the user.
+    """
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
@@ -42,8 +57,8 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_santized_number("Enter the first number: ")
+    num2 = request_santized_number("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
